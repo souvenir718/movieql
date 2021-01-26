c9b1fd12261ee86ea34f87fdb9200c06aa4422d3
souvenir718 <mongkey0@naver.com>
2021-01-26 09:14:31 +0900

    wrapping REST API

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,17 +1,23 @@
 import { people, getById, getMovies, deleteMovie, addMovie } from './db';
+import { getAPIMovies } from './api_wrap_db';
+// const resolvers = {
+//     // Query: {
+//     //     people: () => people,
+//     //     person: (_, { id }) => getById(id),
+//     // },
+//     Query: {
+//         movies: () => getMovies(),
+//         movie: (_, { id }) => getById(id),
+//     },
+//     Mutation: {
+//         addMovie: (_, { name, score }) => addMovie(name, score),
+//         deleteMovie: (_, { name }) => deleteMovie(name),
+//     },
+// };
 
 const resolvers = {
-    // Query: {
-    //     people: () => people,
-    //     person: (_, { id }) => getById(id),
-    // },
     Query: {
-        movies: () => getMovies(),
-        movie: (_, { id }) => getById(id),
-    },
-    Mutation: {
-        addMovie: (_, { name, score }) => addMovie(name, score),
-        deleteMovie: (_, { name }) => deleteMovie(name),
+        movies: (_, { limit, rating }) => getAPIMovies(limit, rating),
     },
 };
 
