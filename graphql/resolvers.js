9181b8d1efa702401699d62326e343c97ad951db
souvenir718 <mongkey0@naver.com>
2021-02-18 09:00:42 +0900

    movie, suggestion 추가

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,21 @@
-import { people, getById, getMovies, deleteMovie, addMovie } from './db';
-import { getAPIMovies } from './api_wrap_db';
+import { getMovies, getMovie, getSuggestions } from './db';
+
 const resolvers = {
-    // Query: {
-    //     people: () => people,
-    //     person: (_, { id }) => getById(id),
-    // },
+    Query: {
+        movies: (_, { rating, limit }) => getMovies(limit, rating),
+        movie: (_, { id }) => getMovie(id),
+        suggestions: (_, { id }) => getSuggestions(id),
+    },
+};
+
+export default resolvers;
+
+/*
+const resolvers = {
+    Query: {
+        people: () => people,
+        person: (_, { id }) => getById(id),
+    },
     Query: {
         movies: () => getMovies(),
         movie: (_, { id }) => getById(id),
@@ -15,10 +26,10 @@ const resolvers = {
     },
 };
 
-// const resolvers = {
-//     Query: {
-//         movies: (_, { limit, rating }) => getAPIMovies(limit, rating),
-//     },
-// };
-
-export default resolvers;
+const resolvers = {
+    Query: {
+        movies: (_, { limit, rating }) => getAPIMovies(limit, rating),
+    },
+};
+*/
+// export default resolvers;
